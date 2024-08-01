fcaa22627e19c201f4ca5e6cad436cf46de51001
RAMJI735 <147968785+RAMJI735@users.noreply.github.com>
2024-08-01 22:49:17 +0530

    page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,12 @@
 import Image from "next/image";
 import Chat from "./api/Chat";
-
+import Navbar from "./components/navbar";
 
 
 export default function Home() {
   return (
 <>
-
+<Navbar/>
 <Chat/>
 
 </>
